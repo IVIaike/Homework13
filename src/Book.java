f6782cbb6bc48e19537858cf96db9e05c8d2a607
IVIaike <trainer.ekb@gmail.com>
2022-12-22 01:08:25 +0500

    Домашняя работа №13 (Методы объектов) на проверку.

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String bookTitle;
     private Author authorBook;
@@ -23,5 +25,24 @@ public class Book {
 
     public void setPublishingYear (int publishingYear) {
         this.publishingYear = publishingYear;
+    }
+
+    public String toString() {
+        return "Название книги: " + this.bookTitle + ", автор " + this.authorBook.getAuthorFirstName() + " " +  this.authorBook.getAuthorLastName() + ", издание " + this.publishingYear + " года.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this.getClass() != o.getClass()) {
+            return false;
+        }
+      if (this == o) { return true;}
+      Book book1 = (Book) o;
+      return Objects.equals(authorBook, bookTitle);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(bookTitle);
     }
 }
